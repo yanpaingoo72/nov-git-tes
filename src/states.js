4671df925292ff4b650b89165872d1de76505b86
yanpaingoo72 <yanpaingoo223@gmail.com>
2024-10-29 19:25:25 +0630

    end v1.0.5

--- src/states.js
+++ src/states.js
@@ -3,5 +3,5 @@ export const fruits = [
     { id: 2, name: 'Banana', price: 200 },
     { id: 3, name: 'Cherry', price: 100 },
     { id: 4, name: 'Date', price: 50 },
-    { id: 5, name: 'Elderberry', price: 300 }
+    { id: 5, name: 'kiwi', price: 300 }
 ];
